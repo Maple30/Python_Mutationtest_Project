6c5cb969900dc00c5e84532623669149cdabbc5b
Maple30 <st0963105799@gmail.com>
2020-10-21 16:44:26 +0800

    what is the project wrongs?????

--- mutation_test_game/shukudai/diff_2/NetGame.py
+++ mutation_test_game/shukudai/diff_2/NetGame.py
@@ -1,15 +1,12 @@
 def game(x):
     p=0
-    if x > 10:
+    if x > 50:
         return 1
-    elif (x-1) > 8:
+    if x > 10:
         return 2
-    elif (x+3) < 0:
+    elif (x-1) > 8:
         return 3
-    elif (x-5) < -10:
+    elif (x+3) < 0:
         return 4
-    elif x < -20:
-        return 5
-    
-    return p
 
+    return p
--- mutation_test_game/shukudai/diff_2/test_diff_2_5.py
+++ mutation_test_game/shukudai/diff_2/test_diff_2_5.py
@@ -6,17 +6,16 @@ def game(x):
         return 2
     elif (x-3) < 0:
         return 3
-    elif (x-40) < -10:
+    elif (x-5) < -10:
         return 4
     elif x < -20:
         return 5
     
     return p
-
-
 def test_game():
-    assert game(2)==4
     assert game(10)==2
-    assert game(9)==4
-    assert game(8)==4
+    assert game(9)==0
+    assert game(8)==0
+    assert game(7)==0
+    assert game(-3)==0
     
