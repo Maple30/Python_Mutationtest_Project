56c64ea3599557fc479b909b716acff41456d1bb
Maple30 <st0963105799@gmail.com>
2020-10-25 17:24:15 +0800

    文件修改

--- pratice.py
+++ pratice.py
@@ -14,7 +14,12 @@ def minima(Num1, Num2):
         return Num2
     else:
         return Num1
+
+
 assert(10,minima(10,20))
+assert(8,minima(10,8))
+assert(-100,minima(-100,20))
+assert(0,minima(10,0))
 
 
 
